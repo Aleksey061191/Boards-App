0ca8b4d47be55b07c54e12d8ccdbec1f39cff619
DESKTOP-MTGDTDN\Aleksey <a.n.makarenka@gmail.com>
2022-05-11 20:28:18 +0300

    feat: add base.scss file, constants.scss, mixins.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import './App.css';
 import { store } from './store/store';
 
 import Root from './pages/Root';
