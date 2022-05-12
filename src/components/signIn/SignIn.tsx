a90ff0557f5f9e10ddad31abebab8bfb729525c2
lena-r-a <rizhankova.elena@gmail.com>
2022-05-12 20:38:49 +0300

    feat: add formik and yup validation

--- src/components/signIn/SignIn.tsx
+++ src/components/signIn/SignIn.tsx
@@ -1,6 +1,20 @@
 import { TextField, Grid, Paper, Avatar, Button } from '@mui/material';
+import { Form, Formik, Field } from 'formik';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
+import * as Yup from 'yup';
 import cl from './SignIn.module.scss';
+
+const INITIAL_SIGNIN_STATE = {
+  email: '',
+  password: '',
+};
+
+const FORM_VALIDATION = Yup.object().shape({
+  email: Yup.string().required('Enter your email').email('Invalid email'),
+  password: Yup.string()
+    .required('Enter the password')
+    .min(7, 'Password should be minimum 7 characters'),
+});
 
 function SignIn(): JSX.Element {
   return (
@@ -10,27 +24,54 @@ function SignIn(): JSX.Element {
           <LockOpenIcon />
         </Avatar>
         Sign In
-        <Grid className={cl.tfClasses}>
-          <TextField
-            required
-            label="E-mail"
-            type="email"
-            autoComplete="E-mail"
-            margin="dense"
-            fullWidth
-          />
-          <TextField
-            required
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            margin="dense"
-            fullWidth
-          />
-          <Button className={cl.btnClasses} variant="contained" fullWidth>
-            Sign In
-          </Button>
-        </Grid>
+        <Formik
+          initialValues={{ ...INITIAL_SIGNIN_STATE }}
+          validationSchema={FORM_VALIDATION}
+          onSubmit={(values, formikHelpers) => {
+            console.log(values);
+            formikHelpers.resetForm();
+          }}
+        >
+          {({ errors, isValid, touched, dirty }) => (
+            <Form className={cl.tfClasses}>
+              <Grid>
+                <Field
+                  name="email"
+                  required
+                  label="E-mail"
+                  as={TextField}
+                  type="email"
+                  autoComplete="E-mail"
+                  margin="dense"
+                  fullWidth
+                  error={Boolean(errors.email) && Boolean(touched.email)}
+                  helperText={Boolean(touched.email) && errors.email}
+                />
+                <Field
+                  required
+                  name="password"
+                  label="Password"
+                  as={TextField}
+                  type="password"
+                  autoComplete="current-password"
+                  margin="dense"
+                  fullWidth
+                  error={Boolean(errors.password) && Boolean(touched.password)}
+                  helperText={Boolean(touched.password) && errors.password}
+                />
+                <Button
+                  className={cl.btnClasses}
+                  variant="contained"
+                  fullWidth
+                  type="submit"
+                  disabled={!dirty || !isValid}
+                >
+                  Sign In
+                </Button>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
       </Paper>
     </Grid>
   );
--- src/components/signUp/SignUp.tsx
+++ src/components/signUp/SignUp.tsx
@@ -1,6 +1,24 @@
 import { TextField, Grid, Paper, Avatar, Button } from '@mui/material';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
+import { Form, Formik, Field } from 'formik';
+import * as Yup from 'yup';
 import cl from './SignUp.module.scss';
+
+const INITIAL_SIGNIN_STATE = {
+  firstname: '',
+  secondname: '',
+  email: '',
+  password: '',
+};
+
+const FORM_VALIDATION = Yup.object().shape({
+  firstname: Yup.string().required('Enter your first name'),
+  secondname: Yup.string().required('Enter your first name'),
+  email: Yup.string().required('Enter your email').email('Invalid email'),
+  password: Yup.string()
+    .required('Enter the password')
+    .min(7, 'Password should be minimum 7 characters'),
+});
 
 function SignUp(): JSX.Element {
   return (
@@ -10,44 +28,78 @@ function SignUp(): JSX.Element {
           <LockOpenIcon />
         </Avatar>
         Sign up
-        <Grid className={cl.nameFields}>
-          <TextField
-            required
-            label="First name"
-            type="text"
-            autoComplete="First Name"
-            margin="dense"
-          />
-          <TextField
-            required
-            id="name1"
-            label="Last name"
-            type="text"
-            autoComplete="Last Name"
-            margin="dense"
-          />
-        </Grid>
-        <Grid className={cl.tfClasses}>
-          <TextField
-            required
-            label="email"
-            type="email"
-            autoComplete="E-mail"
-            margin="dense"
-            fullWidth
-          />
-          <TextField
-            required
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            margin="dense"
-            fullWidth
-          />
-          <Button className={cl.btnClasses} variant="contained" fullWidth>
-            Sign In
-          </Button>
-        </Grid>
+        <Formik
+          initialValues={{ ...INITIAL_SIGNIN_STATE }}
+          validationSchema={FORM_VALIDATION}
+          onSubmit={(values, formikHelpers) => {
+            console.log(values);
+            formikHelpers.resetForm();
+          }}
+        >
+          {({ errors, isValid, touched, dirty }) => (
+            <Form className={cl.tfClasses}>
+              <Grid className={cl.nameFields}>
+                <Field
+                  required
+                  name="firstname"
+                  label="First name"
+                  as={TextField}
+                  type="text"
+                  autoComplete="First Name"
+                  margin="dense"
+                  error={Boolean(errors.firstname) && Boolean(touched.firstname)}
+                  helperText={Boolean(touched.firstname) && errors.firstname}
+                />
+                <Field
+                  required
+                  name="secondname"
+                  label="Second name"
+                  as={TextField}
+                  type="text"
+                  autoComplete="Last Name"
+                  margin="dense"
+                  error={Boolean(errors.secondname) && Boolean(touched.secondname)}
+                  helperText={Boolean(touched.secondname) && errors.secondname}
+                />
+              </Grid>
+              <Grid>
+                <Field
+                  required
+                  name="email"
+                  label="email"
+                  as={TextField}
+                  type="email"
+                  autoComplete="E-mail"
+                  margin="dense"
+                  fullWidth
+                  error={Boolean(errors.email) && Boolean(touched.email)}
+                  helperText={Boolean(touched.email) && errors.email}
+                />
+                <Field
+                  required
+                  name="password"
+                  label="Password"
+                  as={TextField}
+                  type="password"
+                  autoComplete="current-password"
+                  margin="dense"
+                  fullWidth
+                  error={Boolean(errors.password) && Boolean(touched.password)}
+                  helperText={Boolean(touched.password) && errors.password}
+                />
+                <Button
+                  className={cl.btnClasses}
+                  variant="contained"
+                  fullWidth
+                  type="submit"
+                  disabled={!dirty || !isValid}
+                >
+                  Sign In
+                </Button>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
       </Paper>
     </Grid>
   );
