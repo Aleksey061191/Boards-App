5eb8aac5b02a650ee9d720bd8f78336840057d19
lena-r-a <rizhankova.elena@gmail.com>
2022-05-17 12:39:32 +0300

    fix: change hoc RequireAuth for checking if tocken is valid

--- src/components/hoc/RequireAuth.tsx
+++ src/components/hoc/RequireAuth.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from 'react';
 import { useLocation, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store/store';
+import usersApi from '../../services/usersApi';
+import getToken from '../../services/helpers/getToken';
 
 const RequireAuth = ({ children }: { children: JSX.Element }) => {
+  const [isAuth, setIsAuth] = useState(Boolean(getToken()));
   const location = useLocation();
-  const auth = localStorage.getItem('token');
 
-  if (!auth) {
+  async function fetchAPI() {
+    try {
+      await usersApi.getAllUsers();
+    } catch (err) {
+      setIsAuth(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  if (!isAuth) {
     return <Navigate to="/auth" state={{ from: location }} />;
   }
   return children;
